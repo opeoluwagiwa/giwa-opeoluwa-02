bbf18f416c030604f50de380517aab92494bde9a
Opeoluwa Giwa <54221425+opeoluwagiwa@users.noreply.github.com>
2022-07-15 17:45:29 +0000

    Check Challenge 1 & 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
+    let sum = 0;
+     for (let element of arrayOfNumbers) {
+         sum = sum + element;
+     }
+     return sum;
+ }
+assignment.sumOfNumbers = sumOfNumbers;
+
 
 /**
  * Challenge - 2
@@ -28,9 +33,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.countEvenNumbers = countEvenNumbers;
+    let count = 0;
+     for (let element of arrayOfNumbers) {
+         if (element % 2 === 0){
+             count++
+         }
+     }
+     return count;
+ }
+assignment.countEvenNumbers = countEvenNumbers;
+
 
 /**
  * Challenge - 3
